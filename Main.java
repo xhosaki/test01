69a9de5cc2b6a2e575697e962e1831b4cf31e2e7
kanazawa <e1q19024@oit.ac.jp>
2023-01-19 16:33:35 +0900

    Playerの実装

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("SEMonster");
+    System.out.println("SEMonster!");
     Monster monster = new Monster();
+    Player player = new Player();
+    player.drawMonsters();
+    player.showDeck();
     System.out.println(monster.toString());
+
 
   }
 }
