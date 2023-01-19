f28c508086f379b034a5d3eef93dd1531d15adbc
kanazawa <e1q19024@oit.ac.jp>
2023-01-19 16:26:45 +0900

    summon monster処理を実装

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("SEMonster");
+    System.out.println("SEMonster!");
     Monster monster = new Monster();
     System.out.println(monster.toString());
-
   }
 }
+
